6efbe014311ac65813bf29e62364f14730b1ff33
Tsuki <tsukibusiness74@gmail.com>
2023-06-19 19:30:56 -0400

    TEST

---
+++ src/main/java/net/tsukiplus/TsukiPlusClient.java
@@ -0,0 +1,11 @@
+package net.tsukiplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TsukiPlusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
