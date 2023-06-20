220e5fe2c7fb8c0f827781b0478ff27624d0464c
Tsuki <tsukibusiness74@gmail.com>
2023-06-19 22:44:09 -0400

    Add Mod Item framework and Ruby Item

---
+++ src/main/java/net/tsukiplus/item/ModItemGroup.java
@@ -0,0 +1,30 @@
+package net.tsukiplus.item;
+
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.tsukiplus.TsukiPlus;
+
+public class ModItemGroup {
+    public static ItemGroup RUBY = Registry.register(Registries.ITEM_GROUP, new Identifier(TsukiPlus.MOD_ID, "ruby"),
+            FabricItemGroup.builder()
+                    .displayName(Text.literal("Ruby item Group"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                    }).build());
+
+    public static void registerItemGroups() {
+        //adding items to minecraft(existing) groups
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(ModItems.RUBY);
+        });
+    }
+
+}
---
+++ src/main/java/net/tsukiplus/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.tsukiplus.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tsukiplus.TsukiPlus;
+
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TsukiPlus.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TsukiPlus.LOGGER.info("Registering Mod Items for " + TsukiPlus.MOD_ID);
+    }
+
+}
